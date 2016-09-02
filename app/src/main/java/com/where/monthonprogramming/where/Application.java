a5f4cf58dadc26b8a26ef987d3158ee85e165368
Poobest <poobest.pooh@gmail.com>
2016-09-02 22:16:39 +0700

    Project Structure

---
+++ app/src/main/java/com/where/monthonprogramming/where/Application.java
@@ -0,0 +1,16 @@
+package com.where.monthonprogramming.where;
+
+/**
+ * Created by ipoobest on 9/2/2016.
+ */
+public class Application extends android.app.Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+}
---
+++ app/src/main/java/com/where/monthonprogramming/where/activity/SearchActivity.java
@@ -0,0 +1,15 @@
+package com.where.monthonprogramming.where.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.where.monthonprogramming.where.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}

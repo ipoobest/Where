2f59650e23a07f1bcc52029efd8f0a13417a93cb
Poobest <poobest.pooh@gmail.com>
2016-11-10 23:31:46 +0700

    Http manager Singleton

---
+++ app/src/main/java/com/where/monthonprogramming/where/manager/HttpManager.java
@@ -0,0 +1,42 @@
+package com.where.monthonprogramming.where.manager;
+
+import android.content.Context;
+
+import com.where.monthonprogramming.where.Util.Contextor;
+import com.where.monthonprogramming.where.manager.http.ApiService;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+
+public class HttpManager {
+
+    private static HttpManager instance;
+
+    public static HttpManager getInstance() {
+        if (instance == null)
+            instance = new HttpManager();
+        return instance;
+    }
+
+    private Context mContext;
+    private ApiService service;
+
+    private HttpManager() {
+        mContext = Contextor.getInstance().getContext();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                //ใส่ base url ของ service เรา
+                .baseUrl("")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        service = retrofit.create(ApiService.class);
+
+    }
+
+    public ApiService getService() {
+        return service;
+    }
+
+}
---
+++ app/src/main/java/com/where/monthonprogramming/where/manager/SingletonTemplate.java
@@ -0,0 +1,24 @@
+package com.where.monthonprogramming.where.manager;
+
+import android.content.Context;
+
+import com.where.monthonprogramming.where.Util.Contextor;
+
+
+public class SingletonTemplate {
+
+    private static SingletonTemplate instance;
+
+    public static SingletonTemplate getInstance() {
+        if (instance == null)
+            instance = new SingletonTemplate();
+        return instance;
+    }
+
+    private Context mContext;
+
+    private SingletonTemplate() {
+        mContext = Contextor.getInstance().getContext();
+    }
+
+}

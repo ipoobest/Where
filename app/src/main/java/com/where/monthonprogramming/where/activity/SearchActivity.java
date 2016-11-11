adaa9a559dee1baad26bf2229977167f1b95de05
Barenz <monthon993@gmail.com>
2016-11-11 16:47:30 +0700

    *** part Search *** Can handle Search

--- app/src/main/java/com/where/monthonprogramming/where/activity/SearchActivity.java
+++ app/src/main/java/com/where/monthonprogramming/where/activity/SearchActivity.java
@@ -3,6 +3,9 @@ package com.where.monthonprogramming.where.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.where.monthonprogramming.where.R;
 import com.where.monthonprogramming.where.fragment.SearchFragment;
@@ -28,11 +31,9 @@ public class SearchActivity extends AppCompatActivity {
 
             initInstances();
         }
-    }
-
-    private void initInstances() {
-
-
 
     }
+
+    private void initInstances() {}
+
 }
